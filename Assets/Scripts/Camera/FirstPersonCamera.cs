52eb8cc8faff6321ea504431a87b5cb2e387cd9e
Chatchai Saratakij <CSaratakij@gmail.com>
2020-10-27 19:53:36 +0700

    Add : First person controller

---
+++ Assets/Scripts/Camera/FirstPersonCamera.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivity = 10.0f;
+
+    [SerializeField]
+    float sensitivityMultiplier = 1.0f;
+
+    [SerializeField]
+    Vector3 maxAxisRotation = new Vector3(85.0f, 360.0f, 45.0f);
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Vector3 targetOffset;
+
+    Vector3 rotateAxis;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        InputHandler();
+        RotateHandler();
+    }
+
+    void LateUpdate()
+    {
+        FollowTarget();
+    }
+
+    void Initialize()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void InputHandler()
+    {
+        var multipiler = (sensitivity * sensitivityMultiplier);
+
+        rotateAxis.x += -Input.GetAxisRaw("Mouse Y") * multipiler;
+        rotateAxis.y += Input.GetAxisRaw("Mouse X") * multipiler;
+
+        rotateAxis.x = Mathf.Clamp(rotateAxis.x, -maxAxisRotation.x, maxAxisRotation.x);
+
+        bool isReachTheLimitInYAxis = (rotateAxis.y > maxAxisRotation.y) || (rotateAxis.y < -maxAxisRotation.y);
+
+        if (isReachTheLimitInYAxis) {
+            rotateAxis.y -= maxAxisRotation.y;
+        }
+    }
+
+    void RotateHandler()
+    {
+        var resultRotation = Quaternion.Euler(rotateAxis);
+        transform.rotation = resultRotation;
+    }
+
+    void FollowTarget()
+    {
+        transform.position = (target.position + targetOffset);
+    }
+}
+
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Transform referenceAxis;
+
+    [SerializeField]
+    float moveSpeed = 10.0f;
+
+    Vector2 inputVector;
+    Vector3 velocity;
+
+    CharacterController characterController;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        InputHandler();
+        MovementHandler();
+    }
+
+    void Initialize()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void InputHandler()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+        inputVector = Vector2.ClampMagnitude(inputVector, 1.0f);
+    }
+
+    void MovementHandler()
+    {
+        var direction = GetCameraFacing(referenceAxis);
+        velocity = direction * moveSpeed;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    Vector3 GetCameraFacing(Transform referenceAxis)
+    {
+        var forwardDirection = referenceAxis.forward;
+        var rightDirection = referenceAxis.right;
+
+        forwardDirection.y = 0.0f;
+        rightDirection.y = 0.0f;
+
+        forwardDirection = forwardDirection.normalized;
+        rightDirection = rightDirection.normalized;
+
+        return (rightDirection * inputVector.x) + (forwardDirection * inputVector.y);
+    }
+}
+
